f535fac77c6e08bd19d24d76b1eab13f9d3f2a0a
Justin Horein <justinhorein@gmail.com>
2022-05-11 10:38:50 -0700

    fin Landing Page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,27 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className='logo' />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Synth direct trade umami blue bottle deep v quinoa vinyl. Hot chicken etsy post-ironic hell of gluten-free mlkshk taiyaki godard. Etsy hot chicken lo-fi neutra retro, intelligentsia yr selfies tousled gentrify fashion axe.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
