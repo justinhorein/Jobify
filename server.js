7d42337ee081b5ba58fa4334a10cbd8b035db1ff
Justin Horein <justinhorein@gmail.com>
2022-06-12 14:36:41 -0500

    fin login server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 // Passes errors to middleware with no need for try/catch
-import 'express-async-errors' 
+import 'express-async-errors'
+import morgan from 'morgan'
 
 // db and authenticateUser
 import connectDB from './db/connect.js'
@@ -15,6 +16,10 @@ import jobsRouter from './routes/jobsRoutes.js'
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
