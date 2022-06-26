d990aff41ac58e75dd2604b8ef4bccf58279f1fa
Justin Horein <justinhorein@gmail.com>
2022-06-25 23:03:26 -0500

    fin shared layout and install react-icons

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
+
+function Navbar() {
+  return (
+    <Wrapper>
+        <h4>navbar</h4>
+        <FaHome/>
+    </Wrapper>
+  )
+}
+export default Navbar
